d09001cd4bd7ce5c8e13c3253e70329706746fc2
Chris Sheard <chris.sheard@thomsonreuters.com>
2021-11-25 09:26:54 -0800

    Proj(NoteKeeper): Adding NoteKeeper project

---
+++ NoteKeeper/NoteKeeper/Views/ItemsPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using NoteKeeper.Models;
+using NoteKeeper.Views;
+using NoteKeeper.ViewModels;
+
+namespace NoteKeeper.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+            
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (_viewModel.Notes.Count == 0)
+                _viewModel.LoadItemsCommand.Execute(null);
+        }
+
+
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            var item = args.SelectedItem as Item;
+            if (item == null)
+                return;
+
+            await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
+
+            ItemsListView.SelectedItem = null;
+        }
+
+
+        async void AddItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new ItemDetailPage()));
+        }
+
+        async void DeleteItem_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        void SetAppearance()
+        {
+            var appearance = new UINavigationBarAppearance();
+            appearance.ConfigureWithOpaqueBackground();
+
+        }
+
+    }
+}
